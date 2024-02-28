6dc51d2a32f439635bd0d36f6638b6d9a0de972c
Gianluca <gianluca.maragliano@usi.ch>
2024-02-28 15:30:15 +0100

    Thesis: Initial push

--- app/services/payload_service.py
+++ app/services/payload_service.py
@@ -2,8 +2,13 @@ from app.exceptions.xss_exception import XSSException
 
 async def generate_content_from_payload(params, request, templates, validator):
     sanitized_els = []
+    # print("Generating content from payload...")
+    # print(params, request, templates, validator)
+
     for param in params:
         valid, sanitized = validator.validate(param)
+        print("Non-sanitized", param)
+        print("Sanitized", sanitized)
         if not valid:
             raise XSSException()
         sanitized_els.append(sanitized)
